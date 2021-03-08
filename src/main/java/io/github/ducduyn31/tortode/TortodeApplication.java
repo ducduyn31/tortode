d3bdaefd1618a8a732d3793e9432e035e50e19db
Daniel Ng <ducduy.ababat@gmail.com>
2021-03-08 07:43:02 +0700

    init

---
+++ src/main/java/io/github/ducduyn31/tortode/TortodeApplication.java
@@ -0,0 +1,13 @@
+package io.github.ducduyn31.tortode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TortodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TortodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ducduyn31/tortode/TortodeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ducduyn31.tortode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TortodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
